3fccd14fa25d3d221b51f5d8d7542ad81d5d105c
Mateusz <mateuszkoncikowski@users.noreply.github.com>
2024-03-06 09:55:46 +0100

    feat: Add legacy plugin for compatibility with older browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["chrome 42"],
+      modernTargets: ["chrome 42"],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
